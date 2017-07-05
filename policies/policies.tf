d413327cec746c9bae1f2fbc6c0ce249b8c7f740
Justin Carter <justin@starkandwayne.com>
2017-07-05 10:53:17 +0200

    Restructure kit

--- policies/policies.tf
+++ policies/policies.tf
@@ -11,3 +11,8 @@ resource "vault_policy" "nomad-server" {
   name = "nomad-server"
   policy = "${file("${path.module}/nomad-server.hcl")}"
 }
+
+resource "vault_policy" "hashi-ui" {
+  name = "hashi-ui"
+  policy = "${file("${path.module}/hashi-ui.hcl")}"
+}
